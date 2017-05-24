6162c0f909415f426ab367614c5ed875dfa1c0c4
David Waite <dwaite498@gmail.com>
2017-05-24 00:47:16 +0000

    finish functionality

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -21,7 +21,7 @@ var blocmetrics = {};
         var event = {event: { name: eventName }};
         
         var request = new XMLHttpRequest();
-        request.open("POST", "http://localhost:3000/api/events", true);
+        request.open("POST", "http://https://blocmetrics-dwaite498.c9users.io/api/events", true);
         request.setRequestHeader('Content-Type', 'application/json');
         request.send(JSON.stringify(event));
     };
